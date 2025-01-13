ae59a0e25b4f233723d09a0df45a263a863fde3b
Shane-Yang <81016471+qew4476@users.noreply.github.com>
2025-01-13 21:12:51 +0800

    Add register.html, login.html, article_content.html,

--- bigium_app/urls.py
+++ bigium_app/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from . import views
 
 app_name = "Bigium_app"
-urlpatterns = [path('', views.index, name="index")]
+urlpatterns = [path('', views.index, name="index"),
+               path('article/', views.article_content, name="article_content")]
