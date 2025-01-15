799b01910276c473ac52b261663309610654e57b
Shane-Yang <81016471+qew4476@users.noreply.github.com>
2025-01-16 00:14:57 +0800

    feat: Implement login requirement for write_article.html

--- bigium_app/views.py
+++ bigium_app/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,reverse
+from django.contrib.auth.decorators import login_required
+from django.urls import reverse_lazy
+#reverse_lazy: It is useful for when you need to use a URL reversal before your project’s URLConf is loaded.
 
 # Create your views here.
 def index(request):
@@ -7,6 +9,6 @@ def index(request):
 
 def article_content(request):
     return render(request, template_name="article_content.html")
-
+@login_required(login_url=reverse_lazy("bigium_auth_app:login"))
 def write_article(request):
     return render(request,template_name="write_article.html")
