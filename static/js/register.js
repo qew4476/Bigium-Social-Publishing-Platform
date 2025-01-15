e9874f93a2e5aec00c7c495154fa26923eb046e9
Shane-Yang <81016471+qew4476@users.noreply.github.com>
2025-01-15 17:37:45 +0800

    feat: ajax:send captcha (by jquery)

---
+++ static/js/register.js
@@ -0,0 +1,48 @@
+$(function () {
+    function bind_captcha_btn_click() {
+        $("#captcha-btn").click(function (event) {
+            let $this = $(this);
+            let email = $("input[name='email']").val();
+            if (!email) {
+                alert("Please input your email");
+            } else {
+                // disable the button
+                $this.off('click');
+
+                // ajax
+                $.ajax({
+                    url: '/auth/send_email_captcha/',  // 目标 URL
+                    type: 'POST',  // 请求类型
+                    data: { email: email },  // Data to be sent in the request
+                    success: function (response) {
+                        // 请求成功时的回调函数
+                        console.log("Response from server:", response);
+                    },
+                    error: function (xhr, status, error) {
+                        // 请求失败时的回调函数
+                        console.error("Request failed:", error);
+                    }
+                });
+
+
+                // countdown
+                let countdown_second = 5
+                let timer = setInterval(function () {
+                    if (countdown_second <= 0) {
+                        $this.text("Get captcha");
+                        //Don't count down
+                        clearInterval(timer);
+
+                        //enable the button
+                        bind_captcha_btn_click();
+                    } else {
+                        $this.text(countdown_second + "s")
+                        countdown_second--;
+                    }
+                }, 1000)
+            }
+        })
+    }
+
+    bind_captcha_btn_click();
+});
