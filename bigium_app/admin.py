3c54af52082f0988dea0935be3dea1ba993a5ea8
Shane-Yang <81016471+qew4476@users.noreply.github.com>
2025-01-16 19:52:56 +0800

    feat: article admin.py

--- bigium_app/admin.py
+++ bigium_app/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Article,ArticleCategory,ArticleComment
 
 # Register your models here.
+
+class ArticleCategoryAdmin(admin.ModelAdmin):
+    list_display = ['category']
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ['title','content','publish_time','category','author']
+
+@admin.register(ArticleComment)
+class ArticleCommentAdmin(admin.ModelAdmin):
+    list_display = ['comment', 'publish_time', 'author', 'article']
+
+
+admin.site.register(ArticleCategory,ArticleCategoryAdmin)
+
+
+
+
+
--- bigium_app/models.py
+++ bigium_app/models.py
@@ -3,8 +3,10 @@ from django.db import models
 
 
 class ArticleCategory(models.Model):
-    name = models.CharField(max_length=200)
+    category = models.CharField(max_length=200)
 
+    def __str__(self):
+        return self.category
 
 class Article(models.Model):
     title = models.CharField(max_length=200)
@@ -13,10 +15,15 @@ class Article(models.Model):
     category = models.ForeignKey(ArticleCategory, on_delete=models.CASCADE)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.title
 
 class ArticleComment(models.Model):
     """Comment to the specific article(foreign key)"""
-    content = models.TextField()
+    comment = models.TextField()
     publish_time = models.DateTimeField(auto_now_add=True)
     article = models.ForeignKey(Article, on_delete=models.CASCADE)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.comment
